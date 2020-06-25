4baba0314d6eb75ecb269d3c558a788723a321cb
Michael Fitzmaurice <Michael.Fitzmaurice@arup.com>
2020-06-25 16:03:01 +0100

    Install dependencies from requiremets file in setup

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 """Packaging settings."""
+import os
 
 from setuptools import find_packages, setup
+
+requirementPath="requirements.txt"
+install_requires = []
+if os.path.isfile(requirementPath):
+    with open(requirementPath) as f:
+        install_requires = f.read().splitlines()
 
 setup(
     name="lps",
     version=0.0,
     description="A command line tool for synthesising populations.",
     packages=find_packages(exclude="tests*"),
+    install_requires=install_requires,
     entry_points={"console_scripts": ["lps = lps.main:cli"]},
 )
